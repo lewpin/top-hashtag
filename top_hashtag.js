3cdc7555573de21313d1ceb2537db9b4c5dbd8ee
Brian McClain <brianmmcclain@gmail.com>
2012-03-06 20:09:20 -0500

    Removed now unneeded check for entities field

--- top_hashtag.js
+++ top_hashtag.js
@@ -4,10 +4,8 @@ var twitter = require('twitter-stream');
 var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
 twitter.public(config.username, config.password, function(tweet) {
-	if (tweet.entities) {
-		// Iterate over each hashtag
-		tweet.entities.hashtags.forEach(function(hashtag) {
-			console.log(hashtag.text);
-		})
-	}
+	// Iterate over each hashtag
+	tweet.entities.hashtags.forEach(function(hashtag) {
+		console.log(hashtag.text);
+	})
 });
