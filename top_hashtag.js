3ff3737224764ef6ba9e55300b42d87c7e16da25
Brian McClain <brianmmcclain@gmail.com>
2012-03-06 20:01:52 -0500

    Streams all hashtags

--- top_hashtag.js
+++ top_hashtag.js
@@ -4,5 +4,10 @@ var twitter = require('twitter-stream');
 var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
 twitter.public(config.username, config.password, function(tweet) {
-	console.log(tweet);
+	if (tweet.entities) {
+		// Iterate over each hashtag
+		tweet.entities.hashtags.forEach(function(hashtag) {
+			console.log(hashtag.text);
+		})
+	}
 });
