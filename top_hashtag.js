263c3a1d5449420e7069aac31240e343a12f9938
Brian McClain <brianmmcclain@gmail.com>
2012-03-06 19:54:33 -0500

    Added code to stream tweets

--- top_hashtag.js
+++ top_hashtag.js
@@ -1,1 +1,8 @@
+var fs = require('fs');
 var twitter = require('twitter-stream');
+
+var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+
+twitter.public(config.username, config.password, function(tweet) {
+	console.log(tweet);
+});
